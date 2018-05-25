617c391d87c66a40df50a869adf5979ecc30b9e2
Konstantin Nagula <kostyannagula@gmail.com>
2018-05-25 15:20:33 +0300

    Create routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,47 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import { DemoFormSkuBuilderComponent } from './demo-form-sku-builder/demo-form-sku-builder.component';
 import { FormNgModelComponent } from './form-ng-model/form-ng-model.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import {ProtectedComponent} from './protected/protected.component';
+import {ProductComponent} from './product/product.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'contacts', redirectTo: 'contact'},
+
+    // { path: 'login', component: LoginComponent},
+    //
+    // { path: 'protected', component: ProtectedComponent},
+    // { path: 'products', component: ProductComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoFormSkuComponent,
     DemoFormSkuBuilderComponent,
-    FormNgModelComponent
+    FormNgModelComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
