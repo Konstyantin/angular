6d07ecb39fd38ba3f32e882cf379eb67d19ef432
Konstantin Nagula <kostyannagula@gmail.com>
2018-05-25 14:34:41 +0300

    Created form with ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import { DemoFormSkuBuilderComponent } from './demo-form-sku-builder/demo-form-sku-builder.component';
+import { FormNgModelComponent } from './form-ng-model/form-ng-model.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoFormSkuComponent,
-    DemoFormSkuBuilderComponent
+    DemoFormSkuBuilderComponent,
+    FormNgModelComponent
   ],
   imports: [
     BrowserModule,
