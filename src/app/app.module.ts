fdd093c6dfe0b6eb4f9d14a31344aeb97b37386c
Konstantin Nagula <kostyannagula@gmail.com>
2018-05-29 17:54:49 +0300

    Created Product component and update routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import { DemoFormSkuBuilderComponent } from './demo-form-sku-builder/demo-form-sku-builder.component';
 import { FormNgModelComponent } from './form-ng-model/form-ng-model.component';
@@ -13,6 +13,7 @@ import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import {ProtectedComponent} from './protected/protected.component';
 import {ProductComponent} from './product/product.component';
+import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -20,11 +21,12 @@ const routes: Routes = [
     { path: 'about', component: AboutComponent},
     { path: 'contact', component: ContactComponent},
     { path: 'contacts', redirectTo: 'contact'},
-
-    // { path: 'login', component: LoginComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'product/:id', component: ProductComponent},
     //
-    // { path: 'protected', component: ProtectedComponent},
+    // { path: 'protected', component: ProtectedComponent, canActivate: [ Logged]},
     // { path: 'products', component: ProductComponent}
+    // { path: 'products', component: ProductComponent, children: []}
 ];
 
 @NgModule({
@@ -36,7 +38,8 @@ const routes: Routes = [
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,7 +48,12 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     RouterModule
   ],
-  providers: [],
+  providers: [
+      {provide: LocationStrategy, useClass: HashLocationStrategy},
+      {provide: APP_BASE_HREF, useValue: '/'},
+      // AUTH_PROVIDERS,
+      // LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
 
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  constructor(private route: ActivatedRoute) {
+    route.params.subscribe(params => {this.id = params['id']});
+
+    console.log(this.id);
+  }
 
   ngOnInit() {
   }
